9d44833e88fb512fcd7528a6a3901e3ba55233a6
finalfire <f.cauteruccio@gmail.com>
2021-06-19 21:42:34 +0200

    forward

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use rand::Rng;
+use std::env;
+use std::str::FromStr;
+
 struct Individual {
     chromosome: Vec<u8>,
     size: usize
@@ -7,16 +11,25 @@ struct Population {
     individuals: Vec<Individual>
 }
 
+#[derive(Debug)]
+struct Transaction {
+    items: Vec<u8>,
+    utilities: Vec<u32>,
+    utility: u32
+}
+
+struct Database {
+    transactions: Vec<Transaction>
+}
+
 impl Individual {
     fn new(size: &usize) -> Individual {
+        let mut rng = rand::thread_rng();
         let chromosome: Vec<u8> = (0..*size)
-            .map(|_| 0)
+            .map(|_| if rng.gen::<f32>() >= 0.5 { 1 } else { 0 })
             .collect();
         
-        Individual {
-            chromosome: chromosome,
-            size: *size
-        }
+        Individual { chromosome: chromosome, size: *size }
     }
 }
 
@@ -30,9 +43,31 @@ impl Population {
     }
 }
 
-fn main() {
-    let population = Population::new(&10, &5);
-    for individual in population.individuals {
-        println!("{:?}", individual.chromosome);
+impl Transaction {
+    fn from_str(data: &str, size: &usize) -> Transaction {
+        let mut items: Vec<u8> = vec![0; *size];
+        let mut utilities: Vec<u32> = vec![0; *size];
+
+        let parts: Vec<&str> = data.split(':').collect();
+        let utility = u32::from_str(parts[1]).expect("Error parsing the total utility.");
+        for (item_index, utility_value) in parts[0].split(' ').zip(parts[2].split(' ')) {
+            let index = usize::from_str(item_index).expect("Error parsing the item index.");
+            items[index - 1] = 1;
+            utilities[index -1] = u32::from_str(utility_value).expect("Error parsing the utility value.");
+        }
+
+        Transaction { items, utilities, utility }
     }
+}
+
+impl Database {
+    fn from_file(path: &str) -> Database {
+        
+    }
+}
+
+fn main() {
+    let min_util = 10;
+    let t = Transaction::from_str(&"2 3 4:9:2 2 5", &4);
+    println!("{:?}", t);
 }
